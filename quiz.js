c115ba14bbc4bb244ab0e402d83e059309ba874c
Neliaxni <106556751+Neliaxni@users.noreply.github.com>
2022-06-04 12:56:16 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,6 @@
-
+function 1(){
+	 	 document.getElementById("hallo").value="Great job, that's right";
+  	}
+function 2(){
+	 	 document.getElementById("hallo").value="Sadly no, the right answer is "ARMY"";
+  	}
