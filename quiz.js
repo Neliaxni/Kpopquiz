189d440615f260a576f2fb13e71cda8e3cff86f3
Neliaxni <106556751+Neliaxni@users.noreply.github.com>
2022-06-04 14:26:41 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,12 +2,12 @@ function right(){
 	 	 document.getElementById("Answer").value="Great job, that's right";
   	}
 function wrong(){
-	 	 document.getElementById("Answer").value="Sadly false";
+	 	 document.getElementById("Answer").value="Sorry, but that's wrong ";
   	}
 
 function hello(){
 	 	 document.getElementById("Answ").value="Great job, that's right";
   	}
 function bye(){
-	 	 document.getElementById("Answ").value="Sadly false";
+	 	 document.getElementById("Answ").value="Sorry, but that's wrong";
   	}
