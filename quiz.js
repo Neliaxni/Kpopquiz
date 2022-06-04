7ce5146140a0c486a50ed024a24ea9f9ba309ee2
Neliaxni <106556751+Neliaxni@users.noreply.github.com>
2022-06-04 12:59:10 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
 function 1(){
-	 	 document.getElementById("hallo").value="Great job, that's right";
+	 	 document.getElementById("Answer").value="Great job, that's right";
   	}
 function 2(){
-	 	 document.getElementById("hallo").value="Sadly no, the right answer is "ARMY"";
+	 	 document.getElementById("Answer").value="Sadly no, the right answer is "ARMY"";
   	}
