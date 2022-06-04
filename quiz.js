027fa1fcaca220c49343fb37d9fa5966d0144368
Neliaxni <106556751+Neliaxni@users.noreply.github.com>
2022-06-04 13:37:35 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -4,3 +4,10 @@ function right(){
 function wrong(){
 	 	 document.getElementById("Answer").value="Sadly false";
   	}
+
+function correct(){
+	 	 document.getElementById("Answ").value="Great job, that's right";
+  	}
+function false(){
+	 	 document.getElementById("Answ").value="Sadly false";
+  	}
