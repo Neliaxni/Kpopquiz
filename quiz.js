d05d22ecdeb44b383db0c804a8f59dd3cc651e37
Neliaxni <106556751+Neliaxni@users.noreply.github.com>
2022-06-05 21:56:43 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,41 +1,41 @@
 function right(){
-	 	 document.getElementById("Answer").value="Great job, that's right";
-  	}
+	 	 document.getElementById("Answer").value="Great job, that's right";}
 function wrong(){
 	 	 document.getElementById("Answer").value="Sorry, but that's wrong ";
   	}
 
 function hello(){
-	 	 document.getElementById("Answ").value="Great job, that's right";
-  	}
+	 	 document.getElementById("Answ").value="Great job, that's right";}
 function bye(){
 	 	 document.getElementById("Answ").value="Sorry, but that's wrong";
   	}
 
 function richtig(){
-	 	 document.getElementById("Ans").value="Great job, that's right";
-  	}
+	 	 document.getElementById("Ans").value="Great job, that's right";}
 function falsch(){
 	 	 document.getElementById("Ans").value="Sorry, but that's wrong";
   	}
 
 function richtigg(){
-	 	 document.getElementById("Anss").value="Great job, that's right";
-  	}
+	 	 document.getElementById("Anss").value="Great job, that's right";}
 function falschh(){
 	 	 document.getElementById("Anss").value="Sorry, but that's wrong";
   	}
 
 function richti(){
-	 	 document.getElementById("An").value="Great job, that's right";
-  	}
+	 	 document.getElementById("An").value="Great job, that's right";}
 function falsc(){
 	 	 document.getElementById("An").value="Sorry, but that's wrong";
   	}
 
 function richt(){
-	 	 document.getElementById("A").value="Great job, that's right";
-  	}
+	 	 document.getElementById("A").value="Great job, that's right";}
 function fals(){
 	 	 document.getElementById("A").value="Sorry, but that's wrong";
+  	}
+
+function rich(){
+	 	 document.getElementById("Aa").value="Great job, that's right";}
+function fal(){
+	 	 document.getElementById("Aa").value="Sorry, but that's wrong";
   	}
