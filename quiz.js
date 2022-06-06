6d92cc604ec1c3bf0971794b1b1bdcccb5078b23
Neliaxni <106556751+Neliaxni@users.noreply.github.com>
2022-06-06 08:22:30 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,41 +1,41 @@
 function a(){
-	 	 document.getElementById("Answer").value="Great job, that's right";}
+	 	 document.getElementById("A").value="Great job, that's right";}
 function aa(){
-	 	 document.getElementById("Answer").value="Sorry, but that's wrong ";
+	 	 document.getElementById("A").value="Sorry, but that's wrong ";
   	}
 
 function b(){
-	 	 document.getElementById("Answ").value="Great job, that's right";}
+	 	 document.getElementById("B").value="Great job, that's right";}
 function bb(){
-	 	 document.getElementById("Answ").value="Sorry, but that's wrong";
+	 	 document.getElementById("B").value="Sorry, but that's wrong";
   	}
 
 function c(){
-	 	 document.getElementById("Ans").value="Great job, that's right";}
+	 	 document.getElementById("C").value="Great job, that's right";}
 function cc(){
-	 	 document.getElementById("Ans").value="Sorry, but that's wrong";
+	 	 document.getElementById("C").value="Sorry, but that's wrong";
   	}
 
 function d(){
-	 	 document.getElementById("Anss").value="Great job, that's right";}
+	 	 document.getElementById("D").value="Great job, that's right";}
 function dd(){
-	 	 document.getElementById("Anss").value="Sorry, but that's wrong";
+	 	 document.getElementById("D").value="Sorry, but that's wrong";
   	}
 
 function e(){
-	 	 document.getElementById("An").value="Great job, that's right";}
+	 	 document.getElementById("E").value="Great job, that's right";}
 function ee(){
-	 	 document.getElementById("An").value="Sorry, but that's wrong";
+	 	 document.getElementById("E").value="Sorry, but that's wrong";
   	}
 
 function f(){
-	 	 document.getElementById("Antwort").value="Great job, that's right";}
+	 	 document.getElementById("F").value="Great job, that's right";}
 function ff(){
-	 	 document.getElementById("Antwort").value="Sorry, but that's wrong";
+	 	 document.getElementById("F").value="Sorry, but that's wrong";
   	}
 
 function g(){
-	 	 document.getElementById("Aa").value="Great job, that's right";}
+	 	 document.getElementById("G").value="Great job, that's right";}
 function gg(){
-	 	 document.getElementById("Aa").value="Sorry, but that's wrong";
+	 	 document.getElementById("G").value="Sorry, but that's wrong";
   	}
